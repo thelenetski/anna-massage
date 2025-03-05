8525a0fee2b25a5fe140cf15cce6b15afcb9508b
thelenetski <49438367+thelenetski@users.noreply.github.com>
2025-03-05 19:11:19 +0200

    add style fix animation

--- src/components/Price/Price.jsx
+++ src/components/Price/Price.jsx
@@ -8,31 +8,33 @@ const Price = () => {
         <p>Вартість</p>
       </div>
       <div className={css.priceBox}>
-        <p>Антицелюлітний медовий/спортивний:</p>
+        <p className={css.priceName}>Антицелюлітний медовий/спортивний:</p>
         <div className={css.prices}>
-          <p>650/45хв</p>
-          <p>750/1год</p>
-          <p>1050/1.5год</p>
+          <p>650₴ 45хв</p>
+          <p>750₴ 1год</p>
+          <p>1050₴ 1.5год</p>
         </div>
       </div>
       <div className={css.priceBox}>
-        <p>Загальний класичний:</p>
+        <p className={css.priceName}>Загальний класичний:</p>
         <div className={css.prices}>
-          <p>600/45хв</p>
-          <p>700/1год</p>
-          <p>1000/1.5год</p>
-          <p>1300/2год</p>
+          <p>600₴ 45хв</p>
+          <p>700₴ 1год</p>
+          <p>1000₴ 1.5год</p>
+          <p>1300₴ 2год</p>
         </div>
       </div>
       <div className={css.priceBox}>
-        <p>Дитячий:</p>
+        <p className={css.priceName}>Дитячий:</p>
         <div className={css.prices}>
-          <p>500/30хв</p>
+          <p>500₴ 30хв</p>
         </div>
       </div>
       <div className={css.priceBox}>
-        <p>Нейро-моделюючий іспанський масаж обличчя:</p>
-        <p>400/30хв</p>
+        <p className={css.priceName}>
+          Нейро-моделюючий іспанський масаж обличчя:
+        </p>
+        <p>400₴ 30хв</p>
       </div>
     </div>
   );

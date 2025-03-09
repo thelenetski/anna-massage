1780c9440e0422a3a0b443e0378db59395edd99e
thelenetski <49438367+thelenetski@users.noreply.github.com>
2025-03-09 23:26:06 +0200

    change rotate flower

--- src/components/Price/Price.jsx
+++ src/components/Price/Price.jsx
@@ -2,58 +2,60 @@ import css from "./Price.module.css";
 
 const Price = () => {
   return (
-    <div className={css.priceWrap}>
+    <>
       {/* <div className={css.priceTitles}>
         <p>Тип масажу</p>
         <p>Вартість</p>
       </div> */}
-      <div className={css.priceBox}>
-        <p className={css.priceName} style={{ marginTop: 0 }}>
-          Антицелюлітний, медовий, спортивний:
-        </p>
-        <div className={css.prices}>
-          <p>45хв</p>
-          <p>650₴</p>
-          <p>1год</p>
-          <p>750₴</p>
-          <p>1.5год</p>
-          <p>1050₴</p>
-          <p>2год</p>
-          <p>1350₴</p>
+      <div className={css.priceWrap}>
+        <div className={css.priceBox}>
+          <p className={css.priceName} style={{ marginTop: 0 }}>
+            Антицелюлітний, медовий, спортивний:
+          </p>
+          <div className={css.prices}>
+            <p>45хв</p>
+            <p>650₴</p>
+            <p>1год</p>
+            <p>750₴</p>
+            <p>1.5год</p>
+            <p>1050₴</p>
+            <p>2год</p>
+            <p>1350₴</p>
+          </div>
+        </div>
+        <div className={css.priceBox}>
+          <p className={css.priceName}>Загальний класичний вакуумний:</p>
+          <div className={css.prices}>
+            <p>45хв</p>
+            <p>600₴</p>
+            <p>1год</p>
+            <p>700₴</p>
+            <p>1.5год</p>
+            <p>1000₴</p>
+            <p>2год</p>
+            <p>1300₴</p>
+          </div>
+        </div>
+        <div className={css.priceBox}>
+          <p className={css.priceName}>Дитячий:</p>
+          <div className={css.prices}>
+            <p>30хв</p>
+            <p>500₴</p>
+          </div>
+        </div>
+        <div className={css.priceBox}>
+          <p className={css.priceName}>
+            Нейро-моделюючий іспанський масаж обличчя:
+          </p>
+          <div className={css.prices}>
+            <p>30хв</p>
+            <p>400₴</p>
+            <p>1год</p>
+            <p>750₴</p>
+          </div>
         </div>
       </div>
-      <div className={css.priceBox}>
-        <p className={css.priceName}>Загальний класичний вакуумний:</p>
-        <div className={css.prices}>
-          <p>45хв</p>
-          <p>600₴</p>
-          <p>1год</p>
-          <p>700₴</p>
-          <p>1.5год</p>
-          <p>1000₴</p>
-          <p>2год</p>
-          <p>1300₴</p>
-        </div>
-      </div>
-      <div className={css.priceBox}>
-        <p className={css.priceName}>Дитячий:</p>
-        <div className={css.prices}>
-          <p>30хв</p>
-          <p>500₴</p>
-        </div>
-      </div>
-      <div className={css.priceBox}>
-        <p className={css.priceName}>
-          Нейро-моделюючий іспанський масаж обличчя:
-        </p>
-        <div className={css.prices}>
-          <p>30хв</p>
-          <p>400₴</p>
-          <p>1год</p>
-          <p>750₴</p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
